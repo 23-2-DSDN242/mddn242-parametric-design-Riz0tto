b84f3d597a8a69e50e30673a8f21980eba5126e5
Riz0tto <104946042+Riz0tto@users.noreply.github.com>
2023-09-14 01:50:44 +1200

    Some commenting

--- draw_letters.js
+++ draw_letters.js
@@ -101,26 +101,35 @@ function drawLetter(letterData) {
 
 function interpolate_letter(percent, oldObj, newObj) {
   let new_letter = {};
+
+  // cyan square
   new_letter["sizeC"]    = map(percent, 0, 100, oldObj["sizeC"], newObj["sizeC"]);
   new_letter["rotC"]    = map(percent, 0, 90, oldObj["rotC"], newObj["rotC"]);
   new_letter["offsetCx"] = map(percent, -100,  100, oldObj["offsetCx"], newObj["offsetCx"]);
   new_letter["offsetCy"] = map(percent, -100, 100, oldObj["offsetCy"], newObj["offsetCy"]);
   new_letter["roundC"]    = map(percent, 0, 100, oldObj["roundC"], newObj["roundC"]);
+
+  // magenta square
   new_letter["sizeM"]    = map(percent, 0, 100, oldObj["sizeM"], newObj["sizeM"]);
   new_letter["rotM"]    = map(percent, 0, 90, oldObj["rotM"], newObj["rotM"]);
   new_letter["offsetMx"] = map(percent, -100,  100, oldObj["offsetMx"], newObj["offsetMx"]);
   new_letter["offsetMy"] = map(percent, -100, 100, oldObj["offsetMy"], newObj["offsetMy"]);
   new_letter["roundM"]    = map(percent, 0, 100, oldObj["roundM"], newObj["roundM"]);
+
+  //yellow square
   new_letter["sizeY"]    = map(percent, 0, 100, oldObj["sizeY"], newObj["sizeY"]);
   new_letter["rotY"]    = map(percent, 0, 90, oldObj["rotY"], newObj["rotY"]);
   new_letter["offsetYx"] = map(percent, -100,  100, oldObj["offsetYx"], newObj["offsetYx"]);
   new_letter["offsetYy"] = map(percent, -100, 100, oldObj["offsetYy"], newObj["offsetYy"]);
   new_letter["roundY"]    = map(percent, 0, 100, oldObj["roundY"], newObj["roundY"]);
+
+  // lines
   new_letter["lineBaseX"]    = map(percent, 0, 100, oldObj["lineBaseX"], newObj["lineBaseX"]);
   new_letter["lineBaseY"]    = map(percent, -100, 100, oldObj["lineBaseY"], newObj["lineBaseY"]);
   new_letter["lineLength"]    = map(percent, -100, 100, oldObj["lineLength"], newObj["lineLength"]);
   new_letter["linesAngle"]    = map(percent, 0, 90, oldObj["linesAngle"], newObj["linesAngle"]);
   new_letter["linesRot"]    = map(percent, 0, 90, oldObj["linesRot"], newObj["linesRot"]);
+
   return new_letter;
 }
 
